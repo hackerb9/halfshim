1c37bfc751edf10e4d0a44a8f46f4469e82a52a5
b9 <hackerb9@member.fsf.org>
2026-01-05 23:01:54 -0800

    Remove mistaken idea

--- friends/hdosshim/rst7.asm
+++ friends/hdosshim/rst7.asm
@@ -1,6 +1,8 @@
 ; HDOS System Call Vector
 ; Redirects RST 7 (RST 38H) to the resident HDOS handler.
+	;;  This doesn't work as address 0038 isn't writeable.
 	ORG 0038H
 	JMP 2011H    ; Jump to HDOS SCALL handler entry point
-
+	
+	HLT
 	
